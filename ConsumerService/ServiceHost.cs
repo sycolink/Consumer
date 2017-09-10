bfecb0d5b7f47a7e12d126ae3e23f2666e588298
Lunch <vassiliou.thomas@gmail.com>
2017-09-10 12:18:30 +0100

    Basic RabbitMQ consumer

---
+++ ConsumerService/ServiceHost.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using System.Reflection;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ConsumerService
+{
+    class ServiceHost
+    {
+        private readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ServiceHost()
+        {
+        }
+
+        public void Start()
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+
+                        _log.InfoFormat("Message recieved: [{0}]", message);
+                    };
+
+                    channel.BasicConsume(queue: "hello",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
